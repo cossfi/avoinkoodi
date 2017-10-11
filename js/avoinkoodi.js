cb9dad7279c4d7ee9aec9fe6ab38701e7292709b
Noora Huotari <noora.huotari@coss.fi>
2017-10-11 22:16:21 +0300

    Update avoinkoodi.js

--- js/avoinkoodi.js
+++ js/avoinkoodi.js
@@ -3,7 +3,8 @@
 (function ($) {
   var $projects = $("#projects");
   $projects.html("<tr><td colspan='4'>Ladataan sisältöä...</td></tr>");
-
+  var $eduprojects = $("#eduprojects");
+  $eduprojects.html("<tr><td colspan='4'>Ladataan sisältöä...</td></tr>");
 	// Load projects
 
   $.getJSON("projects.json", function(data) {
@@ -36,6 +37,38 @@
       });
 
       $projects.html(content.join(""));
+  });
+	
+  $.getJSON("eduprojects.json", function(data) {
+      $eduprojects.html("");
+
+      data.projects.sort(function(a, b) {
+          var ownerA = a.owner.toLowerCase();
+          var ownerB = b.owner.toLowerCase();
+          return ownerA.localeCompare(ownerB);
+      });
+
+      var content = data.projects.map(function (project) {
+          var codeUrl = '-',
+              serviceUrl = '-';
+
+          if (project.code_url.length > 1) {
+              codeUrl = "<a href='" + $("<td>").text(project.code_url).html() + "'>Näytä lähdekoodi &raquo;</a>";
+          }
+
+          if (project.service_url.length > 1) {
+              serviceUrl = "<a href='" + $("<td>").text(project.service_url).html() + "'>Siirry palveluun &raquo;</a>";
+          }
+
+          return "<tr>" +
+                 "<td>" + $("<td>").text(project.owner).html() + "</td>" +
+                 "<td>" + $("<td>").text(project.project).html() + "</td>" +
+                 "<td>" + codeUrl + "</td>" +
+                 "<td>" + serviceUrl + "</td>" +
+                 "</tr>";
+      });
+
+      $eduprojects.html(content.join(""));
   });
 
   // Register events
